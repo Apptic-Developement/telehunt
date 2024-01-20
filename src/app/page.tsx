a56d02b2817fa04f5dccac7da62adc9f48dd0c50
Pranoy Majumdar <kumarricky252@gmail.com>
2024-01-20 15:37:55 +0000

    Added new ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,37 +3,42 @@ import { FaSearch } from "react-icons/fa";
 import cards from "@/lib/cards.json";
 import CategoryGrid from "@/components/utils/category-grid";
 import { GrChannel, GrGroup, GrRobot } from "react-icons/gr";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center container md:my-12 my-20 px-4 h-full w-full">
+    <main className="container mx-auto lg:px-16 md:px-10 px-8 my-10">
 
-      <section className="flex flex-col items-center gap-3 text-center md:mb-20 mb-24">
-        <h1 className="md:text-3xl text-2xl font-semibold">
-          Discover numerous Telegram bots, channels, groups!
-        </h1>
-
-        <form className="flex items-center bg-card py-2 px-4 border-2 rounded-md md:w-[80%] w-full">
-          <input
-            className="bg-transparent focus-visible:outline-none w-full"
-            type="text"
-            placeholder="Search..."
-          />
-          <Button size="icon" variant="ghost">
-            <FaSearch />
-          </Button>
+      <section className="flex flex-col gap-4">
+        <h1 className="md:text-2xl text-xl font-bold">Discover numerous Telegram bots, channels, groups!</h1>
+        <form className="flex items-center gap-2">
+          <Input className="h-14" placeholder="Search..." />
+          <Button variant="secondary" className="h-14"><FaSearch /></Button>
         </form>
+        <div className="flex gap-2 mobile:flex-wrap justify-between">
+          <Tabs defaultValue="channels" className="w-[400px] mobile:w-full">
+            <TabsList className="w-fit mobile:w-full">
+              <TabsTrigger value="channels">Channels</TabsTrigger>
+              <TabsTrigger value="groups">Groups</TabsTrigger>
+              <TabsTrigger value="bots">Bots</TabsTrigger>
+            </TabsList>
+          </Tabs>
+          <Popover>
+            <PopoverTrigger className="w-fit mobile:w-full">
+              <Button className="w-fit mobile:w-full" variant="outline">Filter</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+            </PopoverContent>
+          </Popover>
+        </div>
+
       </section>
 
-      <section className="flex flex-col space-y-16">
-        {/* Channels */}
-        <CategoryGrid name="Popular Channels" icon={GrChannel} data={cards} />
-        {/* Groups */}
-        <CategoryGrid name="Popular Groups" icon={GrGroup} data={cards} />
-        {/* Bots */}
-        <CategoryGrid name="Popular Bots" icon={GrRobot} data={cards} />
-      </section>
+
+
     </main>
   );
 }
